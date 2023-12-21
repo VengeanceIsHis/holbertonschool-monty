12e4e015f585ace7f12df8ffa1493337eaf66fec
VengeanceIsHis <elijahjpoole@gmail.com>
2023-12-21 08:46:53 -0800

    fix

---
+++ exec.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+int exec(char *content, stack_t **stack, unsigned int line_number, FILE *file)
+{
+  instruction_t op[] = {
+			{"push", push_op},
+			{"pall", pall_op}
+  };
+  unsigned int i = 0;
+  char *oper;
+  oper = strtok(content, " \n\t");
+  if (oper && oper[0] == '#')
+    return (0);
+  bus.arg = strtok(NULL, " \n\t");
+  while (op[i].opcode && op)
+    {
+      if (strcmp(oper, op[i].opcode) == 0)
+	{
+	  op[i].f(stack, counter);
+	  return (0);
+	}
+      i++;
+    }
+  if (oper && op[i].opcode == NULL)
+    {
+      fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
+      flose(file);
+      free(content);
+      free_stack(*stack);
+      exit(EXIT_FAILURE);
+      return (1);
+    }
+}
