db5d582a4497023fa357f0e20dc4d8630f62b3e6
VengeanceIsHis <elijahjpoole@gmail.com>
2023-12-21 09:43:18 -0800

    fix

--- monty.h
+++ monty.h
@@ -40,7 +40,6 @@ typedef struct bus_s
 extern bus_t bus;
 void addqueue(stack_t **head, int n);
 void q(stack_t **head, unsigned int line_number);
-void (*get_op(char **input))(stack_t **stack, unsigned int line_number);
 void push_op(stack_t **stack, unsigned int line_number);
 void pall_op(stack_t **stack, unsigned int line_number);
 void addnode(stack_t **stack, int n);
