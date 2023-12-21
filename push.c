49a8661f00743ab6b008b32bf4413b2076bf2925
VengeanceIsHis <elijahjpoole@gmail.com>
2023-12-21 10:03:02 -0800

    fix

--- push.c
+++ push.c
@@ -4,11 +4,11 @@ void push_op(stack_t **stack, unsigned int line_number)
   int n, j = 0, flag = 0;
   if (bus.arg)
     {
-      if (bus.arg[0] == '-')
+      if (bus.arg && bus.arg[0] == '-')
 	j++;
       for (; bus.arg[j] != '\0'; j++)
 	{
-	  if (bus.arg[j] > 57 || bus.arg[j] < 48)
+	  if (bus.arg[j] > '9' || bus.arg[j] < '0')
 	    flag = 1;
 	}
       if (flag == 1)
